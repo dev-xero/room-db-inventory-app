213f606b8743df719e3dd25c1a4e7618f0cdba11
Xero kun <elo48858@gmail.com>
2023-07-01 14:45:47 +0100

    `FEAT` Implemented items repository

--- app/src/main/java/com/example/inventory/data/OfflineItemsRepository.kt
+++ app/src/main/java/com/example/inventory/data/OfflineItemsRepository.kt
@@ -19,13 +19,23 @@ package com.example.inventory.data
 import kotlinx.coroutines.flow.Flow
 
 class OfflineItemsRepository(private val itemDao: ItemDao) : ItemsRepository {
-    override fun getAllItemsStream(): Flow<List<Item>> = itemDao.getAllItems()
+    override fun getAllItemsStream(): Flow<List<Item>> {
+        return itemDao.getAllItems()
+    }
 
-    override fun getItemStream(id: Int): Flow<Item?> = itemDao.getItem(id)
+    override fun getItemStream(id: Int): Flow<Item?> {
+        return itemDao.getItem(id)
+    }
 
-    override suspend fun insertItem(item: Item) = itemDao.insert(item)
+    override suspend fun insertItem(item: Item) {
+        return itemDao.insert(item)
+    }
 
-    override suspend fun deleteItem(item: Item) = itemDao.delete(item)
+    override suspend fun deleteItem(item: Item) {
+        return itemDao.delete(item)
+    }
 
-    override suspend fun updateItem(item: Item) = itemDao.update(item)
+    override suspend fun updateItem(item: Item) {
+        return itemDao.update(item)
+    }
 }
